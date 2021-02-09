9c15a29d921547ae9cb40a2a417da3d9c9beecc3
dubovsky <dubovsky@plzen.eu>
2021-02-09 14:56:56 +0100

    images

--- deploy-nette-ftp.sh
+++ deploy-nette-ftp.sh
@@ -11,4 +11,4 @@ FTP_PASS=$3
 SOURCE=$4
 TARGET=$5
 
-lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mirror -Rev $SOURCE $TARGET --parallel=10 --exclude-glob .git* --exclude .git/ --exclude temp/sessions/* --exclude www/galleries/* --exclude www/images/*"
+lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mirror -Rnev $SOURCE $TARGET --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/ --exclude temp/sessions/* --exclude www/galleries/* --exclude www/images/*"
